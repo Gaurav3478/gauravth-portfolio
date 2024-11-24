0eb4f929d00e0176cb5bea6f83feb4173700b9be
varsha <varshareddyarrabelli2003@gmail.com>
2024-11-24 16:17:44 +0530

    Added Resume and added navbar transition

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -3,13 +3,14 @@ import './HomePage.css';
 
 const Home = () => {
   return (
-      <div className="home">
-        <h1>Welcome to My Portfolio</h1>
-        <p>Hi, I'm Gaurav. I'm a Software Engineer</p>
-        <button onClick={() => window.scrollTo({ top: document.body.scrollHeight, behavior: 'smooth' })}>
-          Learn More About Me
-        </button>
-      </div>
+    <div className="home">
+      <h1>Welcome to My Portfolio</h1>
+      <p>Hi, I'm Gaurav. I'm a Software Engineer</p>
+      <a href="../../../assets/files/Arrabelli_Varsha_Reddy_Resume_2024.pdf" download>
+        <button type="submit"><span>Download My CV</span><img src="../../../assets/images/Download.png"
+          alt="Download CV"></img></button>
+      </a>
+    </div>
   );
 };
 
